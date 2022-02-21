6c97a217918868108f93e27b7666568210d0df03
kremsnita420 <duranovic.safet@gmail.com>
2022-02-21 12:36:52 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Banner from "./components/Banner";
+import Cases from "./components/Cases";
+import Header from "./components/Header";
 import "./styles/App.scss";
 
 function App() {
+
+  // calculate space for mobile browser's bars on top and bottom
+  useEffect(() => {
+    let vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+  })
+
   return (
     <div className='App'>
-      <h2>Hello this is your starter files </h2>
+      <Header />
+      <Banner />
+      <Cases />
     </div>
   );
 }
